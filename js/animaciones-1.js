3719b75bd7b409486616d22c6f67287f50de57fd
Lucas <lcas83539@gmail.com>
2021-03-16 17:24:35 -0300

    proyecto terminado

---
+++ js/animaciones-1.js
@@ -0,0 +1,91 @@
+let tl = gsap.timeline({ repeat: -1, yoyo: true });
+
+tl.from('.img', {
+    duration: 2,
+    height: 200,
+    width: 300,
+   
+    ease: 'linear'
+    
+});
+let ty = gsap.timeline({ repeat: -1, yoyo: true });
+
+ty.from('.title', {
+    duration: 1,
+    opacity:0,
+   
+    ease: 'linear'
+    
+});
+
+
+window.sr = ScrollReveal();
+    distancia = 2000;
+    distanciaCorta = 1000;
+    time = 300;
+    sr.reveal('.animacion-1', {
+        delay: time,
+        duration: 3000,
+        origin: 'left',
+        distance: '-1000px'
+        
+    });
+    time += distancia
+    sr.reveal('.animacion-2', {
+        delay: time,
+        duration: 2500,
+        origin: 'right',
+        distance: '-500px'
+        
+    });
+    time += distancia
+    sr.reveal('.animacion-3', {
+        delay:time,
+        duration: 1500,
+        origin: 'top',
+        distance: '-100px'
+        
+    });
+    time += distancia
+    sr.reveal('.animacion-4', {
+        delay:time,
+        duration: 1500,
+        origin: 'top',
+        distance: '-100px'
+        
+    });
+    time += distancia
+    sr.reveal('.animacion-5', {
+        delay:time,
+        duration: 1500,
+        origin: 'bottom',
+        distance: '-10px'
+        
+    });
+    time += distanciaCorta
+    sr.reveal('.animacion-6', {
+        delay:time,
+        duration: 1500,
+        origin: 'bottom',
+        distance: '-10px'
+        
+    });
+    time += distanciaCorta
+    sr.reveal('.animacion-7', {
+        delay:time,
+        duration: 1500,
+        origin: 'bottom',
+        distance: '-10px'
+        
+    });
+    time += distanciaCorta
+    sr.reveal('.animacion-8', {
+        delay:time,
+        duration: 1500,
+        origin: 'bottom',
+        distance: '-10px'
+        
+    });
+
+  
+
